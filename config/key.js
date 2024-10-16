bf6e4aa510c869664c3385115135b5b20e1b1dd4
Monika-Saraswat <saraswatmonika55@gmail.com>
2024-10-16 13:48:28 +0530

    h

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-	MongoURI: "mongodb+srv://saraswatmonika55:ecommerceShoppingApp@cluster0.llxzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+	MongoURI: "mongodb+srv://saraswatmonika55:ecommerceShoppingApp@cluster0.llxzo.mongodb.net/ecommerceShoppingApp?retryWrites=true&w=majority&appName=Cluster0"
     
 }
